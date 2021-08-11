aecaf84eababf1954488860feb654d9105471030
Arno Fiva <arnofiva@gmail.com>
2021-08-11 18:08:43 +0200

    Added camera positions

--- src/index/constants.ts
+++ src/index/constants.ts
@@ -36,8 +36,26 @@ export class Section {
       },
       heading: 55.49,
       tilt: 27.64
+    }),
+    new Camera({
+      position: {
+        longitude: 8.5155129,
+        latitude: 47.40915386,
+        z: 2291.69999
+      },
+      heading: 181.61,
+      tilt: 63.98
+    }),
+    new Camera({
+      position: {
+        longitude: 8.50612225,
+        latitude: 47.29491531,
+        z: 2139.48809
+      },
+      heading: 83.64,
+      tilt: 66.9
     })
-  )
+  );
 
   static readonly GOTTHARD = new Section(
     50, // 50min
@@ -51,42 +69,42 @@ export class Section {
       heading: 37.04,
       tilt: 65.7
     }),
-    // new Camera({
-    //   position: {
-    //     longitude: 8.44483296,
-    //     latitude: 46.96159700,
-    //     z: 8820.48138
-    //   },
-    //   heading: 143.97,
-    //   tilt: 71.19
-    // }),
-    // new Camera({
-    //   position: {
-    //     longitude: 8.76589998,
-    //     latitude: 46.44697671,
-    //     z: 8820.48138
-    //   },
-    //   heading: 144.20,
-    //   tilt: 71.19
-    // })
     new Camera({
       position: {
-        longitude: 8.51109290,
-        latitude: 46.45750037,
-        z: 15605.80733
+        longitude: 8.62930101,
+        latitude: 47.00796764,
+        z: 10259.1925
       },
-      heading: 26.58,
-      tilt: 66.73
+      heading: 188.82,
+      tilt: 68.46
     }),
     new Camera({
       position: {
-        longitude: 9.21189998,
-        latitude: 46.22905008,
-        z: 10283.89300
+        longitude: 8.75380363,
+        latitude: 46.4046695,
+        z: 8386.95846
       },
-      heading: 307.62,
-      tilt: 70.09
+      heading: 141.72,
+      tilt: 69.49
     })
+    // new Camera({
+    //   position: {
+    //     longitude: 8.51109290,
+    //     latitude: 46.45750037,
+    //     z: 15605.80733
+    //   },
+    //   heading: 26.58,
+    //   tilt: 66.73
+    // }),
+    // new Camera({
+    //   position: {
+    //     longitude: 9.21189998,
+    //     latitude: 46.22905008,
+    //     z: 10283.89300
+    //   },
+    //   heading: 307.62,
+    //   tilt: 70.09
+    // })
   );
 
   static readonly CENERI = new Section(
@@ -100,6 +118,24 @@ export class Section {
       },
       heading: 156.83,
       tilt: 68.33
+    }),
+    new Camera({
+      position: {
+        longitude: 8.93383496,
+        latitude: 46.26408764,
+        z: 5468.9504
+      },
+      heading: 156.85,
+      tilt: 68.33
+    }),
+    new Camera({
+      position: {
+        longitude: 8.84061376,
+        latitude: 46.10261438,
+        z: 7029.88396
+      },
+      heading: 139.31,
+      tilt: 57.49
     })
   );
 
@@ -116,8 +152,13 @@ export class Section {
     }
   }
 
-  private constructor(public slowTime: number, public fastTime: number, public camera: Camera, public startCam?: Camera, public endCam?: Camera) { }
-
+  private constructor(
+    public slowTime: number,
+    public fastTime: number,
+    public camera: Camera,
+    public startCam?: Camera,
+    public endCam?: Camera
+  ) {}
 }
 
 
